0756165c46f5cdea6cb4a3d4acfc7e77999eed6f
TrueTinker <kieran.forrester@live.co.uk>
2022-03-09 15:39:11 +0000

    removing ipad code and files

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,6 @@ import { h, Component } from 'preact';
 
 // import required Components from 'components/'
 import Iphone from './iphone';
-import Ipad from './ipad';
 
 export default class App extends Component {
 //var App = React.createClass({
@@ -11,29 +10,15 @@ export default class App extends Component {
 	// once the components are loaded, checks if the url bar has a path with "ipad" in it, if so sets state of tablet to be true
 	componentDidMount() {
 		const urlBar = window.location.href;
-		if (urlBar.includes("ipad")) {
-			this.setState({
-				"isTablet": true
-			});
-		} else {
-			this.setState({
-				"isTablet": false
-			});
-		}
+		this.setState({
+			"isTablet": false
+		});
 	}
 
 	/*
 		A render method to display the required Component on screen (iPhone or iPad) : selected by checking component's isTablet state
 	*/
 	render(){
-		if (this.state.isTablet){
-			return (
-				<div id="app">
-					<Ipad/>
-				</div>
-			);
-		}
-
 		return (
 			<div id="app">
 				<Iphone/>
