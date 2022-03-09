19cb4c87d26ba4c78081cb5f5436a19e21d1102f
Tom K <tomknighton@icloud.com>
2022-03-09 15:15:58 +0000

    Revert "delete wrong stuff and add basic card component"

--- src/components/card/index.js
+++
@@ -1,9 +0,0 @@
-// import preact
-import { h, render, Component } from "preact";
-import style from "./style";
-
-export default class Card extends Component {
-	render() {
-		return <div class={style.card}>Test</div>;
-	}
-}
