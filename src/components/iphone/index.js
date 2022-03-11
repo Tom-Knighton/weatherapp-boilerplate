88403a7a9ebc37b72ad4c549e9cabfd4e4d51eba
TrueTinker <kieran.forrester@live.co.uk>
2022-03-11 13:55:37 +0000

    Created card 4

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -19,68 +19,77 @@ export default class Iphone extends Component {
 		this.setState({ display: true });
 
 		APIClient.fetchWeatherForLocation("London").then((data) => {
-			console.log("CURRENT",data)
-			this.setState({date: data.location.localtime})
+			console.log("CURRENT",data);
+			this.setState({date: data.location.localtime});
 			this.setState({currentTemp: data.current.temp_c});
-			this.setState({currentIcon: "https://".concat(data.current.condition.icon)})
+			this.setState({currentIcon: "https://".concat(data.current.condition.icon)});
 
 		 });
 		APIClient.fetchForecastForLocation("London", 10).then((data) => {
-			console.log("FORECAST",data)
-			this.setState({forecast: data.forecast.forecastday})
+			console.log("FORECAST",data);
+			this.setState({forecast: data.forecast.forecastday});
 
 		});
 		APIClient.searchForLocation("London").then((data) => { console.log("LOCATION",data); });
 		APIClient.getAstronomyData("London").then((data) => { console.log("ASTRONOMY",data); });
 		APIClient.getSportData("London").then((data) => { console.log("SPORTS",data); });
+
+		APIClient.fetchWeatherForLocation("London").then((data) => {
+			this.setState({
+				currentCloud: data.current.cloud,
+				currentWindSpeed: data.current.gust_mph,
+				currentRainfall: data.current.precip_mm,
+				currentVisibility: data.current.vis_miles
+			});
+		});
 	}
 
 	rainToday(){
 
-		let twelveHourCount = 0
+		let twelveHourCount = 0;
 		//starting from the current hour, grab the next twelve hours and report if and when the next rain will come.
 
-			//grab the bit of the forecast we care about
-			let todaysForecast = this.state.forecast[0];
-			todaysForecast = todaysForecast.hour;
-			console.log("FORECAST",todaysForecast);
+		//grab the bit of the forecast we care about
+		let todaysForecast = this.state.forecast[0];
+		todaysForecast = todaysForecast.hour;
+		console.log("FORECAST",todaysForecast);
 
-			// figure out what time to start from
+		// figure out what time to start from
 
-			let hour = (new Date()).getHours()
+		let hour = (new Date()).getHours();
 
-			let returnforecastHour = todaysForecast[hour]
+		let returnforecastHour = todaysForecast[hour];
 
-			//let twelveHourCount = 0
-			//starting from the current hour, grab the next twelve hours and report if and when the next rain will come.
-			//
-			let currentCondition = todaysForecast[hour].condition.text
-			for (let checkHour = hour; checkHour <= 23; checkHour ++){
+		//let twelveHourCount = 0
+		//starting from the current hour, grab the next twelve hours and report if and when the next rain will come.
+		//
+		let currentCondition = todaysForecast[hour].condition.text;
+		for (let checkHour = hour; checkHour <= 23; checkHour ++){
 
-				//console.log("loop", todaysForecast[checkHour] );
-				let forecastHour = todaysForecast[checkHour];
-				console.log("EQUALS",currentCondition,forecastHour.condition.text)
-				if (currentCondition !== forecastHour.condition.text){
-					if (twelveHourCount == 0 || twelveHourCount == 1){
-						//rain within the hour
-						return <p class = {style.rightRainForcast}>{forecastHour.condition.text} <br></br> 1 hour</p>;
-					}
+			//console.log("loop", todaysForecast[checkHour] );
+			let forecastHour = todaysForecast[checkHour];
+			console.log("EQUALS",currentCondition,forecastHour.condition.text);
+			if (currentCondition !== forecastHour.condition.text){
+				if (twelveHourCount == 0 || twelveHourCount == 1){
+					//rain within the hour
+					return <p class = {style.rightRainForcast}>{forecastHour.condition.text} <br></br> 1 hour</p>;
+				}
 				return <p class = {style.rightRainForcast}> {forecastHour.condition.text}  {twelveHourCount} hours</p>;
-				}
-
-				if (twelveHourCount == 12){
-					//The next twelve hours will be the same.
-					return <p class = {style.rightRainForcast}>{currentCondition}  <br></br> All today</p>;
-				}
-				twelveHourCount ++;
-
 			}
-			for (let checkHour = 0; checkHour <= 12; checkHour++){
-				//Check the remaining 12 hours for rain, searching the next day
-				return "NOTYETDONE"
 
-
+			if (twelveHourCount == 12){
+				//The next twelve hours will be the same.
+				return <p class = {style.rightRainForcast}>{currentCondition}  <br></br> All today</p>;
 			}
+			twelveHourCount ++;
+
+		}
+		for (let checkHour = 0; checkHour <= 12; checkHour++){
+			//Check the remaining 12 hours for rain, searching the next day
+			return "NOTYETDONE";
+
+
+		}
 
 	}
 
@@ -101,7 +110,26 @@ export default class Iphone extends Component {
 				</div></Card>
 				<UpcomingWeatherFeaturesCard/>
 				<Card>Test 3</Card>
-				<Card>Test 4</Card>
+				<Card>
+					<div class={style.gridContainer}>
+						<div class={style.gridItem}>
+							<h4>Cloud Coverage:</h4>
+							<p>{this.state.currentCloud}%</p>
+						</div>
+						<div class={style.gridItem}>
+							<h4>Wind Speed:</h4>
+							<p>{this.state.currentWindSpeed} mph</p>
+						</div>
+						<div class={style.gridItem}>
+							<h4>Rainfall:</h4>
+							<p>{this.state.currentRainfall}mm</p>
+						</div>
+						<div class={style.gridItem}>
+							<h4>Visibility:</h4>
+							<p>{this.state.currentVisibility} mi</p>
+						</div>
+					</div>
+				</Card>
 				<Card>Test 5</Card>
 			</div>
 		);
