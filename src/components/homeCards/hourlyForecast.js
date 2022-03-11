ba3ee11a6f8aec5393d18129dd92933659285bef
Tom K <tomknighton@icloud.com>
2022-03-11 14:02:16 +0000

    Scroll and horizontal for hourly forecast

--- src/components/homeCards/hourlyForecast.js
+++ src/components/homeCards/hourlyForecast.js
@@ -8,11 +8,11 @@ export default class HourlyForecast extends Component {
 		super(props);
 
 		APIClient.fetchForecastForLocation("London").then((data) => {
-            
+
 			this.setState({
-				
-				
-                
+
+
+
 				icon2: data.forecast.forecastday[0],
                 icon3: "https://".concat(data.forecast.forecastday[0].hour[2].condition.icon),
                 icon4: "https://".concat(data.forecast.forecastday[0].hour[3].condition.icon),
@@ -20,12 +20,12 @@ export default class HourlyForecast extends Component {
                 time2: data.forecast.forecastday[0].hour[1].time,
                 time3: data.forecast.forecastday[0].hour[3].time,
 
-                
+
 			})
 		});
 
         APIClient.fetchWeatherForLocation("London").then((data) => {
-			
+
 			this.setState({date: data.location.localtime})
 			this.setState({currentTemp: data.current.temp_c});
 			this.setState({currentWeatherIcon: "https://".concat(data.current.condition.icon)})
@@ -46,11 +46,11 @@ export default class HourlyForecast extends Component {
 
 
 		if (hour > 12 && hour < 24){
-            
+
             return <h3>{hour-12}pm</h3>;
 
 		}
-        
+
         if (hour == 0){
             return <h3>{12}am</h3>;
 
@@ -60,12 +60,12 @@ export default class HourlyForecast extends Component {
 
         }
         if (hour < 12){
-            
+
             return <h3>{hour}am</h3>;
 
 		}
         return <h3>{hour-24}am</h3>;
-        
+
     }
 
 
@@ -82,19 +82,19 @@ export default class HourlyForecast extends Component {
 
         //greater than 24 means next day: to do
         if (hour > 24){
-            
+
             return hour-12;
 
 		}
-        
-       
+
+
         return hour
 
     }
     //greater than 24 means next day: to do
     theIcon(toAdd){
         let number = this.hourForIcon(toAdd)
-        
+
         return <img src ={"https://".concat(this.state.icon2.hour[number].condition.icon)} alt = "error" ></img>;
     }
 
@@ -102,9 +102,9 @@ export default class HourlyForecast extends Component {
         render() {
             return (
                 <Card>
-                    
-                    <div class={style.container}>
-                        
+
+                    <div class={style.horizontal}>
+
                         <div class={style.row}>
                             <h3>Now</h3>
                             <img src ={this.state.currentWeatherIcon} alt = "error"></img>
@@ -130,14 +130,10 @@ export default class HourlyForecast extends Component {
                             {this.state.icon2 && this.theIcon(4)}
                         </div>
                     </div>
-                    
+
                 </Card>
             );
         }
 
 
 }
-
-
-
-   
