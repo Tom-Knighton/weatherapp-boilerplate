a3a4f2cd30daa0a2252cb9fca53692013a2d08bb
Tom K <tomknighton@icloud.com>
2022-03-24 13:53:31 +0000

    Add all weather condition to search

--- src/pages/SearchCondition.js
+++ src/pages/SearchCondition.js
@@ -5,7 +5,6 @@ import APIClient from "../lib/APIClient";
 import Button from "../components/button";
 
 export class SearchCondition extends Component {
-
 	constructor(props) {
 		super(props);
 		// Page needs to store, the weather the person wants to search for
@@ -13,13 +12,13 @@ export class SearchCondition extends Component {
 		// A list of the searchResults.
 
 		this.state = {
-			weather: "default"
+			weather: "default",
 		};
 		this.state = {
-			day: new Date()
+			day: new Date(),
 		};
 		this.state = {
-			searchList: []
+			searchList: [],
 		};
 
 		this.onChangeValue = this.onChangeValue.bind(this);
@@ -29,25 +28,22 @@ export class SearchCondition extends Component {
 		//An call to the client to grab the weather forecast for 3 days ahea, limited because of the license used.
 		APIClient.fetchForecastForLocation(7).then((data) => {
 			this.setState({
-				forecast: data.forecast
+				forecast: data.forecast,
 			});
 		});
-
 	}
 	//The events that grab the users choice of weather and date. Date is checked to see if the day can be searched for in the forecast from the APIClient
 	onChangeValue(event) {
 		//
-		this.setState({weather: event.target.value});
+		this.setState({ weather: event.target.value });
 	}
 
 	onChangeValue2(event) {
 		//
-		this.setState({day: event.target.value});
-
-
+		this.setState({ day: event.target.value });
 	}
 
-	searchFunc(){
+	searchFunc() {
 		// searchFunction, changes searchList to contain the searchResults, which is called in the render function to display the results.
 		let current = new Date();
 		current.setHours(0, 0, 0, 0);
@@ -55,33 +51,79 @@ export class SearchCondition extends Component {
 		maxDate.setDate(current.getDate() + 2);
 		let stateDate = new Date(this.state.day);
 
-		if (stateDate.getTime() > maxDate.getTime() || stateDate.getTime() < current.getTime()){
-			alert('Invalid Date! Search is limited to 3 days because of the API license.');
+		if (
+			stateDate.getTime() > maxDate.getTime() ||
+			stateDate.getTime() < current.getTime()
+		) {
+			alert(
+				"Invalid Date! Search is limited to 3 days because of the API license."
+			);
 			return false;
 		}
 
 		let chosenWeather = this.state.weather;
 		this.state.searchList = [];
 		this.state.successfulSearch = true;
-		function matchWeather(weather,match){
+		function matchWeather(weather, match) {
 			//Takes the chosenWeather from the user, and figures out which conditions need to be searched for.
 			//To change, add a new key called the weather condtion to look for, and the value as a list of the substituted weather conditions.
+			if (weather == "All") {
+				return true;
+			}
 
 			let weatherDic = {
-        "Clear": ["Sunny", "Clear"],
-        "Cloudy": ["Partly cloudy", "Cloudy", "Overcast"],
-        "Snow":  ["Blowing snow","Patchy light snow","Light snow",
-        "Patchy moderate snow","Moderate or heavy sleet showers","Moderate or heavy showers of ice pellets","Light showers of ice pellets","Light sleet showers","Ice pellets","Moderate snow","Patchy heavy snow","Heavy snow","Light snow showers",
-      "Moderate or heavy snow showers","Moderate or heavy sleet showers","Patchy light snow with thunder","Moderate or heavy snow with thunder","Light sleet","Moderate or heavy sleet", "Blizzard"],
-        "Rain":["Patchy rain possible","Torrential rain shower","Moderate or heavy rain shower","Heavy rain","Moderate rain at times","Moderate or heavy freezing rain","Light freezing rain","Patchy light rain with thunder","Patchy freezing drizzle possible","Patchy light drizzle","Light drizzle","Freezing drizzle","Heavy freezing drizzle","Light rain shower"],
-        "Lightning" :  ["Thundery outbreaks possible","Patchy light snow with thunder",
-        "Moderate or heavy snow with thunder","Patchy light rain with thunder","Moderate or heavy rain with thunder"],
-        "Foggy" : ["Fog", "Mist","Freezing fog"]
-        }
+				Clear: ["Sunny", "Clear"],
+				Cloudy: ["Partly cloudy", "Cloudy", "Overcast"],
+				Snow: [
+					"Blowing snow",
+					"Patchy light snow",
+					"Light snow",
+					"Patchy moderate snow",
+					"Moderate or heavy sleet showers",
+					"Moderate or heavy showers of ice pellets",
+					"Light showers of ice pellets",
+					"Light sleet showers",
+					"Ice pellets",
+					"Moderate snow",
+					"Patchy heavy snow",
+					"Heavy snow",
+					"Light snow showers",
+					"Moderate or heavy snow showers",
+					"Moderate or heavy sleet showers",
+					"Patchy light snow with thunder",
+					"Moderate or heavy snow with thunder",
+					"Light sleet",
+					"Moderate or heavy sleet",
+					"Blizzard",
+				],
+				Rain: [
+					"Patchy rain possible",
+					"Torrential rain shower",
+					"Moderate or heavy rain shower",
+					"Heavy rain",
+					"Moderate rain at times",
+					"Moderate or heavy freezing rain",
+					"Light freezing rain",
+					"Patchy light rain with thunder",
+					"Patchy freezing drizzle possible",
+					"Patchy light drizzle",
+					"Light drizzle",
+					"Freezing drizzle",
+					"Heavy freezing drizzle",
+					"Light rain shower",
+				],
+				Lightning: [
+					"Thundery outbreaks possible",
+					"Patchy light snow with thunder",
+					"Moderate or heavy snow with thunder",
+					"Patchy light rain with thunder",
+					"Moderate or heavy rain with thunder",
+				],
+				Foggy: ["Fog", "Mist", "Freezing fog"],
+			};
 
-
-			for (let i = 0; i < weatherDic[weather].length; i++){
-				if (match == weatherDic[weather][i]){
+			for (let i = 0; i < weatherDic[weather].length; i++) {
+				if (match == weatherDic[weather][i]) {
 					return true;
 				}
 			}
@@ -103,17 +145,22 @@ export class SearchCondition extends Component {
 
       */
 
-		for (let index = 0; index < forecast.length;index ++){
-			if (!(chosenDay == null || chosenDay == '')){
-				if (chosenDay != forecast[index].date){
+		for (let index = 0; index < forecast.length; index++) {
+			if (!(chosenDay == null || chosenDay == "")) {
+				if (chosenDay != forecast[index].date) {
 					continue;
 				}
 			}
 
-			if (chosenWeather != null){
+			if (chosenWeather != null) {
 				//If filtering by Weather
 				for (let hour = 0; hour <= 23; hour++) {
-					if (matchWeather(chosenWeather,forecast[index].hour[hour].condition.text)){
+					if (
+						matchWeather(
+							chosenWeather,
+							forecast[index].hour[hour].condition.text
+						)
+					) {
 						let htmlCrate = forecast[index].hour[hour];
 						//htmlCrate is a single hour, extract all data needed, time, condition, icon, and wrap in appropriate weather object.
 						toListHour = toListHour.concat(htmlCrate);
@@ -127,59 +174,78 @@ export class SearchCondition extends Component {
 		}
 
 		//Checks if there are no search results.
-		if (toListHour.length == 0){
+		if (toListHour.length == 0) {
 			this.state.successfulSearch = false;
 			this.forceUpdate();
-		}
-		else {
+		} else {
 			this.state.searchList = toListHour;
 			this.forceUpdate();
 		}
-
 	}
-
 
 	render() {
 		return (
 			<div class={style.app}>
-				<div class={style.inputFields} id = 'SearchConditionSearch'>
+				<div class={style.inputFields} id="SearchConditionSearch">
 					<header>
 						<h3>Search by Weather and Day</h3>
 					</header>
-					<hr/>
+					<hr />
 					<h4>Weather</h4>
 					<div onChange={this.onChangeValue}>
-						<div class = {style.parentRadio}>
-							<div class = {style.childRadio}>
+						<div class={style.parentRadio}>
+							<div class={style.childRadio}>
 								<input type="radio" value="Clear" name="weather" /> Clear
-								<br/>
+								<br />
 								<input type="radio" value="Cloudy" name="weather" /> Cloudy
-								<br/>
+								<br />
 								<input type="radio" value="Snow" name="weather" /> Snow
 							</div>
-							<div class = {style.childRadio}>
+							<div class={style.childRadio}>
 								<input type="radio" value="Rain" name="weather" /> Rain
-								<br/>
-								<input type="radio" value="Lightning" name="weather" /> Lightning
-								<br/>
+								<br />
+								<input type="radio" value="Lightning" name="weather" />{" "}
+								Lightning
+								<br />
 								<input type="radio" value="Foggy" name="weather" /> Foggy
-
+								<br/>
+                                <input type="radio" value="All" name="weather" /> All
 							</div>
 						</div>
 					</div>
 					<h4>Day</h4>
 					<div onChange={this.onChangeValue2}>
-						<input type="date" id="start" name="selected-day" min="2022-01-01" max="2022-12-31"/>
+						<input
+							type="date"
+							id="start"
+							name="selected-day"
+							min="2022-01-01"
+							max="2022-12-31"
+						/>
 					</div>
 					<p></p>
-					<Button onClick={() => {this.searchFunc(); }}>Search</Button>
+					<Button
+						onClick={() => {
+							this.searchFunc();
+						}}
+					>
+						Search
+					</Button>
 				</div>
-				<div className={style.searchList}> {
-					this.state.successfulSearch ?
+				<div className={style.searchList}>
+					{" "}
+					{this.state.successfulSearch ? (
 						this.state.searchList.map((hour) => (
-							<Card> { hour.time.split("-")[2].split(" ")[0]}/{parseInt(hour.time.split("-")[1])} {hour.condition.text}  { hour.time.split(" ")[1]} </Card>
+							<Card>
+								{" "}
+								{hour.time.split("-")[2].split(" ")[0]}/
+								{parseInt(hour.time.split("-")[1])} {hour.condition.text}{" "}
+								{hour.time.split(" ")[1]}{" "}
+							</Card>
 						))
-						: <Card>No search results --:-- </Card>}
+					) : (
+						<Card>No search results --:-- </Card>
+					)}
 				</div>
 			</div>
 		);
